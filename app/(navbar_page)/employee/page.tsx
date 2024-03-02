d56bb81c0d10adcb7cd81060c451f67898ed0fe9
auliamey <auliaannandya@gmail.com>
2024-03-02 16:18:03 +0700

    add employee page

--- app/(navbar_page)/employee/page.tsx
+++ app/(navbar_page)/employee/page.tsx
@@ -1,9 +1,14 @@
 
+import React from "react";
+import Employee from "./Employee";
 
 const page = ()=>{
+
     return (        
         <div className="w-full">
-        
+          <Employee>
+
+          </Employee>
         </div>
     )
 }
