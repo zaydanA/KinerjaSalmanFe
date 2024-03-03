01a491b424eb52d25c6641969227f6e1ad3e8658
irsyadnb <irsyadxd@gmail.com>
2024-03-03 20:52:13 +0700

    feat: value passing

--- components/shares/search/Search.tsx
+++ components/shares/search/Search.tsx
@@ -3,7 +3,13 @@
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search({
+  placeholder,
+  setSearchValue,
+}: {
+  placeholder: string;
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+}) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -11,12 +17,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set("query", term);
+      params.set("search", term);
     } else {
-      params.delete("query");
+      params.delete("search");
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300);
+    setSearchValue(term);
+  }, 1000);
 
   return (
     <div className="flex">
