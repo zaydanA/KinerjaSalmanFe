f86ddf1cb2674ad3ff34ab186bbf912eb9780cba
Muhammad Bangkit Dwi Cahyono <bangkitdc@gmail.com>
2024-03-05 02:27:31 +0700

    feat: add sso redirect

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useAPI, useAuth } from '@/contexts';
+import Link from 'next/link'
+import { useEffect } from 'react';
+ 
+export default function NotFound() {
+  // const { user } = useAuth();
+  // const { navigateToSSO } = useAPI();
+
+  // useEffect(() => {
+  //   if (!user) {
+  //     navigateToSSO();
+  //   }
+  // }, []);
+
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  )
+}
