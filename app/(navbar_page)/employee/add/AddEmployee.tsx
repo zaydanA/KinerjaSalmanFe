8f7a3c1d18581b4f5797c24dfefc7da760965b15
auliamey <auliaannandya@gmail.com>
2024-03-10 18:32:23 +0700

    fix: fix addemployee

--- app/(navbar_page)/employee/add/AddEmployee.tsx
+++ app/(navbar_page)/employee/add/AddEmployee.tsx
@@ -2,7 +2,8 @@
 import React, { useState, useEffect } from "react";
 import PersonalDataForm from "./personalDataStep";
 import EmployeeDataForm from "./EmployeeDataStep";
-import { IApiBaseEmployee } from "@/types/employee";
+import PayrollDataForm from "./PayrollDataStep";
+import { BloodType, Gender, LastEducation, MaritalStatus } from "@/enums/enums"
 import { apiBase } from '@/api';
 
 interface FormData {
@@ -14,26 +15,30 @@ interface FormData {
         emergencyNumber: string | null;
         address: string;
         placeOfBirth: string;
-        dateOfBirth: Date | null;
-        gender: string;
-        maritalStatus: string;
-        bloodType: string;
+        dateOfBirth: string;
+        gender: Gender;
+        maritalStatus: MaritalStatus;
+        bloodType: BloodType;
         identityNumber: string | null;
-        lastEducation: string;
+        lastEducation: LastEducation;
+        status: number;
     };
     employeeData: {
         employeeID: string;
-        npwpNumber: string | null;
         department: string | null;
         position: string | null;
-        joinDate: string | null;
-        resignDate: string |null;
+        joinDate: string;
+        resignDate: string;
+    };
+    payrollData: {
+        npwpNumber: string;
     };
 }
 
 interface RequiredFields {
     personalData: string[];
     employeeData: string[];
+    payrollData: string[];
   }
 
 const AddEmployee = () => {
@@ -47,21 +52,24 @@ const AddEmployee = () => {
             emergencyNumber: null,
             address: '',
             placeOfBirth: '',
-            dateOfBirth: null,
-            gender: '',
-            maritalStatus: '',
-            bloodType: '',
+            dateOfBirth: '',
+            gender: Gender.M,
+            maritalStatus: MaritalStatus.SINGLE,
+            bloodType: BloodType.A,
             identityNumber: null,
-            lastEducation: '',
+            lastEducation: LastEducation.TIDAK_SEKOLAH,
+            status: 0,
         },
         employeeData: {
             employeeID: '',
-            npwpNumber: null,
             department: null,
             position: null,
             joinDate: null,
             resignDate: null,
         },
+        payrollData: {
+            npwpNumber: '',
+        }
     });
 
     console.log("data: ", formData)
@@ -69,9 +77,7 @@ const AddEmployee = () => {
     const [isFormValid, setIsFormValid] = useState(false);
 
     const handleNextStep = () => {
-        console.log("hai")
         if (isFormValid) {
-            console.log("hai2")
             setStep(step + 1);
         }
     };
@@ -93,8 +99,9 @@ const AddEmployee = () => {
 
     const isRequiredField = (category, field) => {
         const requiredField = {
-            personalData: [ 'firstName', 'lastName', 'email', 'phoneNumber', 'emergencyNumber', 'address', 'placeOfBirth', 'dateOfBirth', 'gender', 'maritalSTatus', 'bloodType', 'identityNumber', 'lastEducation' ],
-            employeeData: [ 'employeeID', 'npwpNumber', 'deptID', 'positionID', 'joinDate' ]
+            personalData: [ 'firstName', 'lastName', 'email', 'phoneNumber', 'emergencyNumber', 'address', 'placeOfBirth', 'dateOfBirth', 'gender', 'maritalStatus', 'bloodType', 'identityNumber', 'lastEducation', 'status' ],
+            employeeData: [ 'employeeID', 'department', 'position', 'joinDate', 'resignDate' ],
+            payrollData: [ 'npwpNumber' ]
         }
         return requiredField[category]?.includes(field);
     }
@@ -115,6 +122,14 @@ const AddEmployee = () => {
             }
         }
 
+        if (step == 3){
+            for (const field in formData['payrollData']){
+                if (isRequiredField('payrollData', field) && formData['payrollData'][field] === '') {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 
@@ -124,25 +139,54 @@ const AddEmployee = () => {
 
     const handleSubmit = async() => {
         try {
+
+            if(formData.personalData.dateOfBirth instanceof Date){
+                console.log("AAAA")
+            } else {
+                console.log("BBBB")
+            }
+
+            const formattedFormData = {
+                ...formData,
+                personalData: {
+                    ...formData.personalData,
+                    dateOfBirth: formData.personalData.dateOfBirth ? new Date(formData.personalData.dateOfBirth) : null
+                },
+                employeeData: {
+                    ...formData.employeeData,
+                    joinDate: formData.employeeData.joinDate ? new Date(formData.employeeData.joinDate) : null,
+                    resignDate: formData.employeeData.resignDate ? new Date(formData.employeeData.resignDate) : null
+                }
+            };
+
+            
+
             const mappedFormData = {
-                full_name: formData.personalData.firstName + ' ' + formData.personalData.lastName,
-                email: formData.personalData.email,
-                phone_number: formData.personalData.phoneNumber,
-                emergency_number: formData.personalData.emergencyNumber,
-                place_of_birth: formData.personalData.placeOfBirth,
-                date_of_birth: formData.personalData.dateOfBirth,
-                gender: formData.personalData.gender,
-                marital_status: formData.personalData.maritalStatus,
-                blood_type: formData.personalData.bloodType,
-                identity_number: formData.personalData.identityNumber,
-                address: formData.personalData.address,
-                last_education: formData.personalData.lastEducation,
-                employee_id: formData.employeeData.employeeID,
-                npwp_number: formData.employeeData.npwpNumber,
-                dept_id: parseInt(formData.employeeData.department ?? '0'),
-                position_id: parseInt(formData.employeeData.position ?? '0'), 
-                join_date: formData.employeeData.joinDate,
-                password: "abcdef",
+                personal_data: {
+                    full_name: formData.personalData.firstName + ' ' + formData.personalData.lastName,
+                    email: formData.personalData.email,
+                    phone_number: formData.personalData.phoneNumber,
+                    emergency_number: formData.personalData.emergencyNumber,
+                    place_of_birth: formData.personalData.placeOfBirth,
+                    date_of_birth: formattedFormData.personalData.dateOfBirth ? formattedFormData.personalData.dateOfBirth.toISOString() : null,
+                    gender: formData.personalData.gender,
+                    marital_status: formData.personalData.maritalStatus,
+                    blood_type: formData.personalData.bloodType,
+                    identity_number: formData.personalData.identityNumber,
+                    address: formData.personalData.address,
+                    last_education: formData.personalData.lastEducation,
+                    status: formData.personalData.status,
+                },
+                employment_data: {
+                    employee_id: formData.employeeData.employeeID,
+                    dept_id: parseInt(formData.employeeData.department ?? '0'),
+                    position_id: parseInt(formData.employeeData.position ?? '0'), 
+                    join_date: formattedFormData.employeeData.joinDate ? formattedFormData.employeeData.joinDate.toISOString() : null,
+                    resign_date: formattedFormData.employeeData.resignDate ? formattedFormData.employeeData.resignDate.toISOString() : null,
+                },
+                payroll_data: {
+                    npwp_number: formData.payrollData.npwpNumber,
+                }
             };
     
             console.log("Mapped Form Data", mappedFormData);
@@ -160,25 +204,25 @@ const AddEmployee = () => {
             <h1 className="text-2xl font-bold mb-4"> Add Employee </h1>
 
             <ul className="flex justify-between w-1/2 mx-auto">
-  <li className={`flex flex-col items-center justify-center step ${step >= 1 ? 'checked' : ''}`}>
-    <div className={`relative rounded-full h-8 w-8 flex items-center justify-center ${step === 1 ? 'bg-black text-white' : 'bg-transparent border-3 border-black'}`}>
-      <span className={`${step >= 1 ? 'circle-lg' : 'circle'}`}>1</span>
-    </div>
-    <div>Personal data</div>
-  </li>
-  <li className={`flex flex-col items-center justify-center step ${step >= 2 ? 'checked' : ''}`}>
-    <div className={`relative rounded-full h-8 w-8 flex items-center justify-center ${step === 2 ? 'bg-black text-white' : 'bg-transparent border-3 border-black'}`}>
-      <span className={`${step >= 2 ? 'circle-lg' : 'circle'}`}>2</span>
-    </div>
-    <div>Employment data</div>
-  </li>
-  <li className={`flex flex-col items-center justify-center step ${step >= 3 ? 'checked' : ''}`}>
-    <div className={`relative rounded-full h-8 w-8 flex items-center justify-center ${step === 3 ? 'bg-black text-white' : 'bg-transparent border-3 border-black'}`}>
-      <span className={`${step === 3 ? 'circle-lg' : 'circle'}`}>3</span>
-    </div>
-    <div>Payroll data</div>
-  </li>
-</ul>
+                <li className={`flex flex-col items-center justify-center step ${step >= 1 ? 'checked' : ''}`}>
+                    <div className={`relative rounded-full h-8 w-8 flex items-center justify-center ${step === 1 ? 'bg-black text-white' : 'bg-transparent border-3 border-black'}`}>
+                    <span className={`${step >= 1 ? 'circle-lg' : 'circle'}`}>1</span>
+                    </div>
+                    <div>Personal data</div>
+                </li>
+                <li className={`flex flex-col items-center justify-center step ${step >= 2 ? 'checked' : ''}`}>
+                    <div className={`relative rounded-full h-8 w-8 flex items-center justify-center ${step === 2 ? 'bg-black text-white' : 'bg-transparent border-3 border-black'}`}>
+                    <span className={`${step >= 2 ? 'circle-lg' : 'circle'}`}>2</span>
+                    </div>
+                    <div>Employment data</div>
+                </li>
+                <li className={`flex flex-col items-center justify-center step ${step >= 3 ? 'checked' : ''}`}>
+                    <div className={`relative rounded-full h-8 w-8 flex items-center justify-center ${step === 3 ? 'bg-black text-white' : 'bg-transparent border-3 border-black'}`}>
+                    <span className={`${step === 3 ? 'circle-lg' : 'circle'}`}>3</span>
+                    </div>
+                    <div>Payroll data</div>
+                </li>
+            </ul>
 
 
 
@@ -193,6 +237,13 @@ const AddEmployee = () => {
                 <EmployeeDataForm
                     formData={formData.employeeData}
                     handleChange={(name, value) => handleChange({category: 'employeeData', name, value})}
+                />
+            )}
+
+            { step === 3 && (
+                <PayrollDataForm
+                    formData={formData.payrollData}
+                    handleChange={(name, value) => handleChange({category: 'payrollData', name, value})}
                 />
             )}
 
