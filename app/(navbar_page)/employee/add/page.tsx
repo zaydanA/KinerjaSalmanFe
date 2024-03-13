12575c0c57043e0bedcb43471b50da78e5cb14e7
zaydanA <13521104@std.stei.itb.ac.id>
2024-03-14 06:43:11 +0700

    fix minor error

--- app/(navbar_page)/employee/add/page.tsx
+++ app/(navbar_page)/employee/add/page.tsx
@@ -3,11 +3,12 @@ import React, { useState } from "react";
 import Pagination from "@/components/shares/pagination/Pagination";
 import AddEmployee from "../../../../components/privates/employees/add/AddEmployee";
 import ProtectedRoute from "@/app/Rbac";
+import { useAuth } from "@/contexts";
 
 const page = () => {
-
+    const{user} = useAuth();
     return (
-        <ProtectedRoute allowedDept={[1,2]} allowedPos={[1,2]}>
+        <ProtectedRoute allowedDept={[1,2,user?.dept.dept_id]} allowedPos={[1,2]}>
             <AddEmployee/>
         </ProtectedRoute>
     )
