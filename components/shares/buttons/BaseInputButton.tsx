bf0371b1e2d20e953708b3d41a875bc6b44ec2ab
irsyadnb <irsyadxd@gmail.com>
2024-03-15 13:47:56 +0700

    fix: sprint review bug fix and more responsive

--- components/shares/buttons/BaseInputButton.tsx
+++ components/shares/buttons/BaseInputButton.tsx
@@ -1,29 +1,26 @@
-
 type BaseButtonType = {
-  text: string,
-  type?: "default" | "submit" | "underlined"
-  disabled?: boolean,
-  onClick: () => void
+  text: string;
+  type?: "default" | "submit" | "underlined";
+  disabled?: boolean;
+  onClick: () => void;
 };
 
-export default function BaseInputButton({ 
+export default function BaseInputButton({
   text,
   type = "default",
   disabled = false,
-  onClick
+  onClick,
 }: BaseButtonType) {
   return (
     <button
-      disabled={disabled} 
-      className={
-        `
-        ${type === 'default' && !disabled ? 'bg-blue-500 hover:bg-blue-700 text-white' : ''}
-        ${type === 'submit' && !disabled ? 'bg-clr-kinerja-gold hover:bg-clr-kinerja-gold-hover text-white' : ''}
-        ${type === 'underlined' && !disabled ? 'bg-transparent text-gray-500 hover:underline' : ''}
-        ${disabled ? 'bg-gray-100 hover:bg-gray-100 text-gray-300' : '' } 
-        font-medium text-sm py-2 px-4 rounded
-        `
-      }
+      disabled={disabled}
+      className={`
+        ${type === "default" && !disabled ? "bg-blue-500 text-white hover:bg-blue-700" : ""}
+        ${type === "submit" && !disabled ? "bg-clr-kinerja-gold text-white hover:bg-clr-kinerja-gold-hover" : ""}
+        ${type === "underlined" && !disabled ? "bg-transparent text-gray-500 hover:underline" : ""}
+        ${disabled ? "bg-gray-100 text-gray-300 hover:bg-gray-100" : ""} 
+        rounded px-4 py-2 text-sm font-medium max-sm:h-10 max-sm:w-fit max-sm:px-1 max-sm:py-0 max-sm:text-[10px]
+        `}
       onClick={onClick}
     >
       {text}
