53303e7333e50230175244237f6e3047b81dcc76
irsyadnb <irsyadxd@gmail.com>
2024-04-21 10:29:07 +0700

    feat: navbar app, protected route

--- app/(navbar_page)/applications/page.tsx
+++ app/(navbar_page)/applications/page.tsx
@@ -1,11 +1,14 @@
 import ApplicationList from "@/components/privates/apply/ApplicationList";
+import ProtectedRoute from "@/app/Rbac";
 import React from "react";
 
 const page = () => {
   return (
+    <ProtectedRoute allowedPos={[1, 2]}>
     <div className="rounded-md border bg-white px-12 py-10 shadow-md max-sm:p-2">
       <ApplicationList />
     </div>
+    </ProtectedRoute>
   );
 };
 
