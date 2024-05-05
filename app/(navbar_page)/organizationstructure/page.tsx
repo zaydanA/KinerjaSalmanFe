aa6ac3333a1e85c58a2081da3745f925f5c909ac
Muhammad Zaydan Athallah <zaydanathallah@gmail.com>
2024-05-06 00:58:12 +0700

    feat: create organizationStructure

---
+++ app/(navbar_page)/organizationstructure/page.tsx
@@ -0,0 +1,11 @@
+
+import OrganizationStructure from "./OrganizationStructure";
+const page = ()=>{
+
+  return (
+    <div className="w-full border-1 border-gray-300 shadow-md gap-10 rounded-lg py-8 flex flex-col h-full overflow-x-auto">
+        <OrganizationStructure></OrganizationStructure>
+    </div>
+  )
+}
+export default page;
